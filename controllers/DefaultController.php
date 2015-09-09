de2d1bb384d0cb47213abedd7f72ee35333703c0
Vlad <jump.e.r@yandex.ru>
2015-09-09 08:51:22 +0300

    edit path

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -3,11 +3,45 @@
 namespace jumper423\yii2images\controllers;
 
 use yii\web\Controller;
+use yii;
+use jumper423\yii2images\models\Image;
+use \jumper423\yii2images\ModuleTrait;
 
 class DefaultController extends Controller
 {
-    public function actionIndex()
+    use ModuleTrait;
+
+    /**
+     *
+     * All we need is love. No.
+     * We need item (by id or another property) and alias (or images number)
+     * @param $item
+     * @param $alias
+     *
+     */
+    public function actionIndex($item='', $dirtyAlias)
     {
-        return $this->render('index');
+        $dotParts = explode('.', $dirtyAlias);
+        if(!isset($dotParts[1])){
+            throw new \yii\web\HttpException(404, 'Image must have extension');
+        }
+        $dirtyAlias = $dotParts[0];
+
+        $size = isset(explode('_', $dirtyAlias)[1]) ? explode('_', $dirtyAlias)[1] : false;
+        $alias = isset(explode('_', $dirtyAlias)[0]) ? explode('_', $dirtyAlias)[0] : false;
+        $image = $this->getModule()->getImage($item, $alias);
+
+        if($image->getExtension() != $dotParts[1]){
+            throw new \yii\web\HttpException(404, 'Image not found (extenstion)');
+        }
+
+        if($image){
+            $response = Yii::$app->getResponse();
+            $response->headers->set('Content-Type', 'image/jpeg');
+            $response->format = yii\web\Response::FORMAT_RAW;
+            return $image->getContent($size);
+        }else{
+            throw new \yii\web\HttpException(404, 'There is no images');
+        }
     }
 }
